782d97c60ec7dcf322328605be187bc1f7e5e8ad
alizeynnalov44 <mi87TUNJY@code.edu.az>
2024-04-18 12:16:06 +0400

    array.js

---
+++ arayy02/array.js
@@ -0,0 +1,119 @@
+let arr = [
+  {
+    name: "test",
+    key: 1,
+  },
+  {
+    name: "task",
+    key: 2,
+  },
+  {
+    name: "tanqo",
+    key: 3,
+  },
+  {
+    name: "like",
+    key: 4,
+  },
+  {
+    name: "task",
+    key: 5,
+  },
+  {
+    name: "trust",
+    key: 6,
+  },
+  {
+    name: "test",
+    key: 7,
+  },
+  {
+    name: "last",
+    key: 8,
+  },
+  {
+    name: "tanqo",
+    key: 9,
+  },
+  {
+    name: "elephant",
+    key: 10,
+  },
+  {
+    name: "love",
+    key: 11,
+  },
+  {
+    name: "small",
+    key: 12,
+  },
+  {
+    name: "little",
+    key: 13,
+  },
+];
+
+
+//1
+
+/*let Arr1 = arr.filter((item) => item.name.startsWith("t"));
+console.log(Arr1);*/
+
+
+//2 
+
+/*let count = 0
+ arr.forEach((item) => {
+  if (item.name.startsWith("t") && item.name.endsWith("t")){
+    count++;
+  }
+});
+console.log(count);*/
+
+
+
+//3
+
+/*let count = 0
+ arr.forEach((item) => {
+  if (item.name.startsWith("t") && item.name.endsWith("t")){
+    count+=item.key;
+  }
+});
+
+console.log(count);*/
+
+
+//4
+
+/*arr.forEach((item) =>{
+  if(item.name.endsWith("e")){
+    item.name = "SuperDev";
+  }
+});
+console.log(arr);*/
+
+
+//5
+/* let word = 0
+ arr.forEach((item) => {
+  if(item.name.lenght)
+})*/
+
+
+
+//10
+
+/*let count = 0
+arr.forEach((item) => {
+  count+=item.key;
+});
+console.log(count);*/
+
+//8
+
+/*let max = arr.filter((item) => (item.name.lenght === 4))
+let newArr = [];
+console.log(max);*/
+
+
