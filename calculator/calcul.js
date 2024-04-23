0516d389796d678b36d612c832259595b8e6c9de
alizeynnalov44 <mi87TUNJY@code.edu.az>
2024-04-23 12:16:25 -0700

    calcul.html

---
+++ calculator/calcul.js
@@ -0,0 +1,28 @@
+const addition = document.querySelector(".addition")
+const substract = document.querySelector(".substract")
+const multiply = document.querySelector(".multiply")
+const division = document.querySelector(".division")
+const reset = document.querySelector(".reset")
+const sums = document.querySelector(".sums")
+const value1 = document.querySelector(".value1")
+const value2 = document.querySelector(".value2")
+addition.addEventListener("click", (e) => {
+    let sum = +value1.value + +value2.value;
+    sums.textContent = sum
+})
+substract.addEventListener("click", (e) => {
+    let sum = +value1.value - +value2.value;
+    sums.textContent = sum
+})
+multiply.addEventListener("click", (e) => {
+    let sum = +value1.value * +value2.value;
+    sums.textContent = sum
+})
+division.addEventListener("click", (e) => {
+    let sum = +value1.value / +value2.value;
+    sums.textContent = sum
+})
+reset.addEventListener("click", (e) => {
+    value1.value = ""
+    value2.value = ""
+})
